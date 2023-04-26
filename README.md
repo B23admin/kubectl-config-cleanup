d748d1622f8a7a1fe29d3671ce1e4d6bac34e1a6
David Kegley <david.kegley@rstudio.com>
2023-04-26 11:43:12 -0400

    Use go modules and refactor package structure

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # kubectl config-cleanup plugin #
 
-`kubectl config-cleanup` is a plugin for automatically cleaning up your kubeconfig.  Every cloud provider has their own utilities for adding kubernetes cluster credentials to your kubeconfig but they don't offer the ability to clean it up once the cluster is deleted. For those of us who launch and delete multiple clusters per day, it would be useful to have an automated way to clean up old kubeconfig entries. This plugin will attempt to connect to each cluster defined in a context, if the connection succeeds then the user, cluster, and context entry are maintained in the result. Otherwise, the entries are removed.
+`kubectl config-cleanup` is a plugin for automatically cleaning up your kubeconfig.
+Every cloud provider has their own utilities for adding kubernetes cluster credentials 
+to your kubeconfig but they don't offer the ability to clean it up once the cluster is deleted. 
+For those of us who launch and delete multiple clusters per day, it would be useful to have 
+an automated way to clean up old kubeconfig entries. This plugin will attempt to 
+connect to each cluster defined in a context, if the connection succeeds then the user, 
+cluster, and context entry are maintained in the result. Otherwise, the entries are removed.
 
 ```bash
 # prints the cleaned kubeconfig to stdout, similar to running: kubectl config view
@@ -21,7 +27,9 @@ See Known Issues below for details
 
 ### config-cleanup.ignore ###
 
-Add a `~/.kube/config-cleanup.ignore` to specify contexts which should be ignored during cleanup. The associated context, user, and cluster will be maintained in the output. This is useful for long running clusters where the api server is behind a firewall.
+Add a `~/.kube/config-cleanup.ignore` to specify contexts which should be ignored during cleanup.
+The associated context, user, and cluster will be maintained in the output. This is useful for long 
+running clusters where the api server is behind a firewall.
 
 example:
 
@@ -40,14 +48,15 @@ data:
 
 Install with krew: `kubectl krew install config-cleanup`
 
-or download the [latest release binary](https://github.com/b23llc/kubectl-cleanup/releases/latest) for your platform and add it to your `$PATH`
+or download the [latest release binary](https://github.com/b23llc/kubectl-cleanup/releases/latest)
+for your platform and add it to your `$PATH`
 
 
 ## Build from source ##
 
 ```bash
-$ go build cmd/kubectl-config-cleanup.go
-$ mv kubectl-config-cleanup /usr/local/bin/kubectl-config_cleanup
+$ go build main.go -o kubectl-config_cleanup
+$ chmod 750 kubectl-config_cleanup && mv kubectl-config_cleanup /usr/local/bin/kubectl-config_cleanup
 ```
 
 ## Release ##
---
+++ cleanup/cleanup.go
@@ -0,0 +1,429 @@
+package cleanup
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	k8sv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/cli-runtime/pkg/printers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	"k8s.io/client-go/tools/clientcmd/api/latest"
+	"k8s.io/client-go/util/homedir"
+	klog "k8s.io/klog/v2"
+
+	// Load client auth plugins
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	cleanupExample = `
+	# prints the cleaned kubeconfig to stdout, similar to running: kubectl config view
+	%[1]s config-cleanup
+
+	# cleanup and save the result
+	%[1]s config-cleanup --raw > ./kubeconfig-clean.yaml
+
+	# cleanup and print the configs that were removed
+	%[1]s config-cleanup --print-removed --raw > ./kubeconfig-removed.yaml
+
+	# print only the context names that were removed
+	%[1]s config-cleanup --print-removed -o=jsonpath='{ range.contexts[*] }{ .name }{"\n"}'
+`
+)
+
+// Options holds configs used to cleanup a kubeconfig file
+type Options struct {
+	PrintFlags *genericclioptions.PrintFlags
+
+	PrintObject printers.ResourcePrinterFunc
+
+	RawConfig       *clientcmdapi.Config // the starting kubeconfig
+	ResultingConfig *clientcmdapi.Config // holds configs we are keeping
+	CleanedUpConfig *clientcmdapi.Config // holds configs that were removed
+
+	CleanupIgnoreConfig *k8sv1.ConfigMap
+	IgnoreContexts      []string
+	// TODO
+	// IgnoreUsers
+	// IgnoreClusters
+
+	ConnectTimeoutSeconds int
+	KubeconfigPath        string
+	CleanupUsers          bool
+	CleanupClusters       bool
+	PrintRaw              bool
+	PrintRemoved          bool
+
+	genericclioptions.IOStreams
+}
+
+// NewCmdCleanup provides a cobra command wrapping CleanupOptions
+func NewCmdCleanup(in io.Reader, out, errout io.Writer) *cobra.Command {
+	streams := genericclioptions.IOStreams{In: in, Out: out, ErrOut: errout}
+	opts := &Options{
+		PrintFlags: genericclioptions.NewPrintFlags("").WithDefaultOutput("yaml"),
+
+		ConnectTimeoutSeconds: int(10),
+		CleanupClusters:       false,
+		CleanupUsers:          false,
+		PrintRaw:              false,
+		PrintRemoved:          false,
+
+		IOStreams: streams,
+	}
+
+	flagSet := flag.NewFlagSet("config-cleanup", flag.ExitOnError)
+	klog.InitFlags(flagSet)
+
+	cmd := &cobra.Command{
+		Use:          "config-cleanup [flags]",
+		Short:        "Attempts to connect to each cluster defined in contexts and removes the ones that fail",
+		Example:      fmt.Sprintf(cleanupExample, "kubectl"),
+		SilenceUsage: true,
+		RunE: func(c *cobra.Command, args []string) error {
+			if err := opts.Complete(c, args); err != nil {
+				return err
+			}
+			if err := opts.Validate(); err != nil {
+				return err
+			}
+			if err := opts.Run(); err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().AddGoFlagSet(flagSet)
+	cmd.Flags().IntVarP(&opts.ConnectTimeoutSeconds, "timeout", "t", opts.ConnectTimeoutSeconds, "Seconds to wait for a response from the server before continuing cleanup")
+	cmd.Flags().StringVar(&opts.KubeconfigPath, "kubeconfig", opts.KubeconfigPath, "Specify a kubeconfig file to cleanup")
+	cmd.Flags().BoolVar(&opts.CleanupClusters, "clusters", opts.CleanupClusters, "Cleanup cluster entries which are not specified by a context")
+	cmd.Flags().BoolVar(&opts.CleanupUsers, "users", opts.CleanupUsers, "Cleanup user entries which are not specified by a context")
+	cmd.Flags().BoolVar(&opts.PrintRaw, "raw", opts.PrintRaw, "Print the raw contents of the kubeconfig after cleanup, suitable for piping to a new file")
+	cmd.Flags().BoolVar(&opts.PrintRemoved, "print-removed", opts.PrintRemoved, "Print the removed contents of the kubeconfig after cleanup, suitable for piping to a new file")
+	opts.PrintFlags.AddFlags(cmd)
+
+	return cmd
+}
+
+// Validate ensures that all required arguments and flag values are provided
+func (o *Options) Validate() error {
+	return nil
+}
+
+// Complete sets all information required for cleaning up the current KUBECONFIG
+func (o *Options) Complete(cmd *cobra.Command, args []string) error {
+	if len(args) != 0 {
+		return fmt.Errorf("unexpected arguments: %v", args)
+	}
+
+	// Define kubeconfig precedence from lowest to highest
+	// ~/.kube/config -> $KUBECONFIG -> --kubeconfig
+	if o.KubeconfigPath == "" {
+		if home := homedir.HomeDir(); home != "" {
+			o.KubeconfigPath = filepath.Join(home, ".kube", "config")
+		}
+		if envConfig := os.Getenv("KUBECONFIG"); envConfig != "" {
+			o.KubeconfigPath = envConfig
+		}
+	}
+
+	// Parse cleanup.ignore ConfigMap file
+	if home := homedir.HomeDir(); home != "" {
+		data, err := ioutil.ReadFile(filepath.Join(home, ".kube", "config-cleanup.ignore"))
+
+		// Return if the error was anything besides that the file does not exist
+		if err != nil && !os.IsNotExist(err) {
+			return err
+		}
+
+		ignoreconfig, err := loadConfigMap(data)
+		if err != nil {
+			return err
+		}
+		o.CleanupIgnoreConfig = ignoreconfig
+		contexts, ok := ignoreconfig.Data["contexts"]
+		if ok {
+			o.IgnoreContexts = strings.Fields(contexts)
+		}
+	}
+
+	config, err := clientcmd.LoadFromFile(o.KubeconfigPath)
+	if err != nil {
+		return err
+	}
+	o.RawConfig = config
+
+	printer, err := o.PrintFlags.ToPrinter()
+	if err != nil {
+		return err
+	}
+	o.PrintObject = printer.PrintObj
+
+	o.CleanedUpConfig = clientcmdapi.NewConfig()
+	o.ResultingConfig = clientcmdapi.NewConfig()
+	o.CleanedUpConfig.Preferences = o.RawConfig.Preferences
+	o.ResultingConfig.Preferences = o.RawConfig.Preferences
+	o.CleanedUpConfig.Extensions = o.RawConfig.Extensions
+	o.ResultingConfig.Extensions = o.RawConfig.Extensions
+
+	return nil
+}
+
+func testConnection(contexts <-chan string, success chan<- string,
+	failure chan<- string, completed *int32, o *Options) {
+
+	for ctx := range contexts {
+
+		ignore := Contains(o.IgnoreContexts, ctx)
+		if ignore {
+			success <- ctx
+			atomic.AddInt32(completed, 1)
+			continue
+		}
+
+		clientset, err := o.NewRestClientForContext(ctx)
+		if err != nil {
+			klog.Infof("%v", err)
+			failure <- ctx
+			atomic.AddInt32(completed, 1)
+			continue
+		}
+
+		_, err = clientset.Discovery().ServerVersion()
+		if err != nil {
+			klog.Infof("%v", err)
+			failure <- ctx
+		} else {
+			success <- ctx
+		}
+		atomic.AddInt32(completed, 1)
+	}
+}
+
+// Run cleans up the user's current KUBECONFIG and prints the result to stdout
+func (o *Options) Run() error {
+	contexts := make(chan string, 100)
+	success := make(chan string)
+	failure := make(chan string)
+
+	var completed int32
+
+	// TODO: this is arbitrary, add a concurrency limit flag?
+	numWorkers := len(o.RawConfig.Contexts)
+	if numWorkers > 25 {
+		numWorkers = 25
+	}
+	for w := 0; w <= numWorkers; w++ {
+		go testConnection(contexts, success, failure, &completed, o)
+	}
+
+	for ctxname := range o.RawConfig.Contexts {
+		contexts <- ctxname
+	}
+	close(contexts)
+
+	// GH-1: Increase default connection timeout and print progress to stderr
+	go func() {
+		ticker := time.NewTicker(3 * time.Second)
+		for {
+			<-ticker.C
+			if completed == int32(len(o.RawConfig.Contexts)) {
+				klog.Infof("Finished testing %d connections...", completed)
+				close(success)
+				close(failure)
+				return
+			}
+			klog.Infof("Finished testing %d of %d connections...", completed, len(o.RawConfig.Contexts))
+		}
+	}()
+
+	for range o.RawConfig.Contexts {
+		select {
+		case s := <-success:
+			o.keepContext(s)
+		case f := <-failure:
+			o.cleanupContext(f)
+		}
+	}
+
+	zombieClusters := clustersNotSpecifiedByAContext(o.RawConfig)
+	if !o.CleanupClusters {
+		for name, cluster := range zombieClusters {
+			o.ResultingConfig.Clusters[name] = cluster
+		}
+	} else {
+		for name, cluster := range zombieClusters {
+			o.CleanedUpConfig.Clusters[name] = cluster
+		}
+	}
+
+	zombieUsers := usersNotSpecifiedByAContext(o.RawConfig)
+	if !o.CleanupUsers {
+		for name, user := range zombieUsers {
+			o.ResultingConfig.AuthInfos[name] = user
+		}
+	} else {
+		for name, user := range zombieUsers {
+			o.CleanedUpConfig.AuthInfos[name] = user
+		}
+	}
+
+	result := o.ResultingConfig
+	if o.PrintRemoved {
+		result = o.CleanedUpConfig
+	}
+
+	klog.Flush()
+
+	// GH-2: If nothing is left in output then don't print an empty kubeconfig
+	if len(result.Clusters) == 0 && len(result.Contexts) == 0 && len(result.AuthInfos) == 0 {
+		return nil
+	}
+
+	if !o.PrintRaw {
+		clientcmdapi.ShortenConfig(result)
+	}
+
+	convertedObj, err := latest.Scheme.ConvertToVersion(result, latest.ExternalVersion)
+	if err != nil {
+		return err
+	}
+
+	return o.PrintObject(convertedObj, o.Out)
+}
+
+// NewRestClientForContext initializes an API server REST client from a given context
+func (o *Options) NewRestClientForContext(ctxname string) (*kubernetes.Clientset, error) {
+
+	config := clientcmdapi.NewConfig()
+	config.CurrentContext = ctxname
+	context := o.RawConfig.Contexts[ctxname]
+
+	authInfo, ok := o.RawConfig.AuthInfos[context.AuthInfo]
+	if !ok {
+		return nil, fmt.Errorf("AuthInfo not found for context: %s", ctxname)
+	}
+	cluster, ok := o.RawConfig.Clusters[context.Cluster]
+	if !ok {
+		return nil, fmt.Errorf("Cluster not found for context: %s", ctxname)
+	}
+
+	config.Contexts[ctxname] = context
+	config.AuthInfos[context.AuthInfo] = authInfo
+	config.Clusters[context.Cluster] = cluster
+
+	configGetter := kubeConfigGetter(config)
+	restConfig, err := clientcmd.BuildConfigFromKubeconfigGetter(cluster.Server, configGetter)
+	if err != nil {
+		return nil, err
+	}
+	restConfig.Timeout = time.Duration(o.ConnectTimeoutSeconds) * time.Second
+
+	return kubernetes.NewForConfig(restConfig)
+}
+
+func (o *Options) keepContext(ctxname string) {
+	context := o.RawConfig.Contexts[ctxname]
+	o.ResultingConfig.Contexts[ctxname] = context
+	auth, ok := o.RawConfig.AuthInfos[context.AuthInfo]
+	if ok {
+		o.ResultingConfig.AuthInfos[context.AuthInfo] = auth
+	}
+	cluster, ok := o.RawConfig.Clusters[context.Cluster]
+	if ok {
+		o.ResultingConfig.Clusters[context.Cluster] = cluster
+	}
+}
+
+func (o *Options) cleanupContext(ctxname string) {
+	context := o.RawConfig.Contexts[ctxname]
+	o.CleanedUpConfig.Contexts[ctxname] = context
+	auth, ok := o.RawConfig.AuthInfos[context.AuthInfo]
+	if ok {
+		o.CleanedUpConfig.AuthInfos[context.AuthInfo] = auth
+	}
+	cluster, ok := o.RawConfig.Clusters[context.Cluster]
+	if ok {
+		o.CleanedUpConfig.Clusters[context.Cluster] = cluster
+	}
+}
+
+// kubeConfigGetter is a noop which returns a function meeting the kubeconfigGetter interface
+// which we can use to initialize a rest client with the provided authInfo
+// ref: https://github.com/kubernetes/contrib/blob/fbb1430dbec659c81b8a0f7492d14f7caeab7505/kubeform/pkg/provider/provider.go#L300
+func kubeConfigGetter(config *clientcmdapi.Config) clientcmd.KubeconfigGetter {
+	return func() (*clientcmdapi.Config, error) {
+		return config, nil
+	}
+}
+
+// loadConfigMap takes a byte slice and deserializes the contents into ConfigMap object.
+func loadConfigMap(data []byte) (*k8sv1.ConfigMap, error) {
+	config := &k8sv1.ConfigMap{}
+	decoded, _, err := latest.Codec.Decode(data, &schema.GroupVersionKind{Version: latest.Version, Kind: "ConfigMap"}, config)
+	if err != nil {
+		return nil, err
+	}
+	return decoded.(*k8sv1.ConfigMap), nil
+}
+
+func clustersNotSpecifiedByAContext(rawconfig *clientcmdapi.Config) map[string]*clientcmdapi.Cluster {
+	clustersInUse := []string{}
+	for _, context := range rawconfig.Contexts {
+		clustersInUse = append(clustersInUse, context.Cluster)
+	}
+	allClusters := []string{}
+	for name := range rawconfig.Clusters {
+		allClusters = append(allClusters, name)
+	}
+	zombies := map[string]*clientcmdapi.Cluster{}
+	for _, c := range allClusters {
+		if !Contains(clustersInUse, c) {
+			zombies[c] = rawconfig.Clusters[c]
+		}
+	}
+	return zombies
+}
+
+func usersNotSpecifiedByAContext(rawconfig *clientcmdapi.Config) map[string]*clientcmdapi.AuthInfo {
+	authInfosInUse := []string{}
+	for _, context := range rawconfig.Contexts {
+		authInfosInUse = append(authInfosInUse, context.AuthInfo)
+	}
+	allAuthInfos := []string{}
+	for name := range rawconfig.AuthInfos {
+		allAuthInfos = append(allAuthInfos, name)
+	}
+	zombies := map[string]*clientcmdapi.AuthInfo{}
+	for _, a := range allAuthInfos {
+		if !Contains(authInfosInUse, a) {
+			zombies[a] = rawconfig.AuthInfos[a]
+		}
+	}
+	return zombies
+}
+
+// Contains util, whether str x is in the slice a
+func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
